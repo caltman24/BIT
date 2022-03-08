2b51f4f1b22950141b94940e1d992c68d00721fd
Corbyn Altman <corbyn.altman01@gmail.com>
2022-03-07 22:15:59 -0500

    working weather command

--- commands/weather.js
+++ commands/weather.js
@@ -4,6 +4,7 @@ const fetch = require("cross-fetch");
 const API_KEY = process.env.API_KEY;
 
 const fetchWeather = async (zip) => {
+  try {
     const res = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${zip}&aqi=no`
     );
@@ -14,7 +15,10 @@ const fetchWeather = async (zip) => {
     } else {
       console.log("Not Successful", res.status);
     }
-  };
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -27,7 +31,62 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const zip = interaction.options.getInteger("zip");
-    interaction.reply("Work in progress")
+    const zip = interaction.options.getInteger("zip").toString();
+    try {
+      if (zip.length !== 5)
+        await interaction.reply("Invalid Input: Must contain 5 numbers");
+      else {
+        await fetchWeather(zip).then((data) => {
+          let weather = {
+            location: {},
+            current: {},
+          };
+
+          for (let i in data) {
+            if (i === "location") {
+              weather.location.name = data[i]["name"];
+              weather.location.region = data[i]["region"];
+              weather.location.country = data[i]["country"];
+              //console.log(`${location.name}, ${location.region}, ${location.country}`);
+            } else if (i === "current") {
+              weather.current.temp_c = data[i]["temp_c"].toString();
+              weather.current.temp_f = data[i]["temp_f"].toString();
+
+              weather.current.wind_mph = data[i]["wind_mph"].toString();
+              weather.current.wind_dir = data[i]["wind_dir"];
+
+              weather.current.feelslike_c = data[i]["feelslike_c"].toString();
+              weather.current.feelslike_f = data[i]["feelslike_f"].toString();
+
+              weather.current.conditionText = data[i]["condition"].text;
+              weather.current.conditionIcon = data[i]["condition"].icon;
+            }
+          }
+
+          const { name, region, country } = weather.location;
+          const {
+            temp_c,
+            temp_f,
+            wind_mph,
+            wind_dir,
+            feelslike_c,
+            feelslike_f,
+            conditionText,
+            conditionIcon,
+          } = weather.current;
+
+          const degreeCode = "\u00B0";
+
+          const locationString = `${name}, ${region}, ${country}`;
+          const tempString = `${temp_f}${degreeCode}F (${temp_c}${degreeCode}C)\nFeels Like: ${feelslike_f}${degreeCode}F (${feelslike_c}${degreeCode}C)`;
+          const windString = `Winds: ${wind_mph}mph ${wind_dir}`;
+          const conditionString = `${conditionText} - ${conditionIcon}`;
+
+          interaction.reply(`\n${locationString}\n${tempString}\n${conditionString}\n${windString}\n`);
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
