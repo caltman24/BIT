b2457df2ee8021f050747a50e9f53ee06a55de8f
Corbyn Altman <corbyn.altman01@gmail.com>
2022-03-09 12:56:19 -0500

    added embed feature

--- commands/weather.js
+++ commands/weather.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Message } = require("discord.js");
 require("dotenv").config();
 const fetch = require("cross-fetch");
 const API_KEY = process.env.API_KEY;
@@ -29,12 +30,17 @@ module.exports = {
         .setDescription("zip code to location")
         .setRequired(true)
     ),
+
   async execute(interaction) {
     const zip = interaction.options.getInteger("zip").toString();
+    const embed = new MessageEmbed().setColor("#00ff99");
+
     try {
-      if (zip.length !== 5)
-        await interaction.reply("Invalid Input: Must contain 5 numbers");
-      else {
+      if (zip.length !== 5) {
+        embed.setTitle("Invalid Input");
+        embed.setDescription("Must contain 5 numbers");
+        await interaction.reply({ embeds: [embed] });
+      } else {
         await interaction.deferReply();
         await fetchWeather(zip).then((data) => {
           if (!data) interaction.editReply("No matching location");
@@ -80,14 +86,21 @@ module.exports = {
 
             const locationString = `${name}, ${region}, ${country}`;
             const tempString = `${temp_f}${degreeCode}F (${temp_c}${degreeCode}C)\nFeels Like: ${feelslike_f}${degreeCode}F (${feelslike_c}${degreeCode}C)`;
-            const windString = `Winds: ${wind_mph}mph ${wind_dir}`;
-            const conditionString = `${conditionText} - ${conditionIcon}`;
 
             //interaction.reply(`\n${locationString}\n${tempString}\n${conditionString}\n${windString}\n`);
-
-            interaction.editReply(
-              `\n${locationString}\n${tempString}\n${conditionString}\n${windString}\n`
+            embed.setTitle(locationString);
+            embed.setThumbnail("https:" + conditionIcon);
+            embed.addFields(
+              { name: "Temperature", value: tempString },
+              {
+                name: "Winds",
+                value: `${wind_mph}mph ${wind_dir}`,
+                inline: true,
+              },
+              { name: "Condition", value: conditionText, inline: true }
             );
+            embed.setTimestamp();
+            interaction.editReply({ embeds: [embed] });
           }
         });
       }
