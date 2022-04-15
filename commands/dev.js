7169b8cd067c9cd39c5b1b5f917365add0b5a1dc
Corbyn Altman <corbyn.altman01@gmail.com>
2022-04-15 03:54:26 -0400

    added dev dashboard

--- commands/dev.js
+++ commands/dev.js
@@ -10,14 +10,17 @@ module.exports = {
     const embedColor = "#00ff99";
     const embed = new MessageEmbed()
       .setColor(embedColor)
-      .setTitle("Developer Stats");
+      .setTitle("Developer Stats")
+      .setDescription(interaction.commandId);
 
     const guildSize = interaction.client.guilds.cache.size.toString();
     const userSize = interaction.client.users.cache.size.toString();
 
     embed.addFields(
-      { name: "Guild Cache Size:", value: guildSize },
-      { name: "Users Cache Size:", value: userSize }
+      { name: "Total Guilds:", value: guildSize },
+      { name: "Total Users:", value: userSize },
+      { name: "User ID:", value: interaction.user.id },
+      { name: "Current Guild ID:", value: interaction.guild.id },
     );
     await interaction.reply({ embeds: [embed], ephemeral: true });
   },
