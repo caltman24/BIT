a3f38bf9e47d8427a4b0ae1e864d2bfc25801881
Corbyn Altman <corbyn.altman01@gmail.com>
2022-04-16 23:58:38 -0400

    added greeting

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,13 +1,38 @@
 module.exports = {
     name: "guildCreate",
     async execute(guild) {
-        guild.systemChannel.send("Thanks for inviting me, type `/help` to get started!")
-        console.log(
-            `[ Joined Guild ] {\n` +
-              `    Guild: ${guild.name}\n` +
-              `    GuildID: ${guild.id}\n` +
-              `}`
-          );
-        
-    }
-}
+      const { Permissions } = require("discord.js");
+      // Get bot from members cache
+      const bot = await guild.members.cache.find(
+        (member) => member.user.username === "BIT_TEST" && member.user.bot
+      );
+  
+      // Create a new role with admin permissions
+      const newRole = await guild.roles.create({
+        name: "Bot",
+        color: "#00ff99",
+        permissions: [Permissions.FLAGS.ADMINISTRATOR],
+      });
+  
+      // Add role to the bot
+      bot.roles.add(newRole);
+  
+      // Set permissions to all commands
+      const fullPermissions = require("../permissions.js");
+      await guild?.commands.permissions.set({ fullPermissions });
+  
+      // Send greeting message on guild join
+      await guild.systemChannel?.send(
+        "Thanks for inviting me, type `/help` to get started!"
+      );
+  
+      // Log
+      console.log(
+        `[ Joined Guild ] {\n` +
+          `    Guild: ${guild.name}\n` +
+          `    GuildID: ${guild.id}\n` +
+          `}`
+      );
+    },
+  };
+  
